631438ca8cc65544f9a01c59cbfad148f25b222c
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2023-11-19 20:31:26 +0100

    More chmod exercises

--- chmod_simple.sh
+++ chmod_simple.sh
@@ -13,31 +13,36 @@ root="$PWD" # get absolute path
 rand_dir() { find "$1" -type d | pick_random; }
 rand_cd() { cd "$(rand_dir "$root")"; }
 
-## perms (symbolic)
-filename="$(rand_touch)"
-chmod "$(random_perm)" "$filename"
-perms="$(stat -c'%A' "$filename")"
-chmod "$(random_perm)" "$filename"
-
-nonce="$(printf '%s' "$perms" | sha256sum | xxd -r -p | base64 | take 8)"
-token_format="$(token_format 1-1 "$nonce")"
-
-make_check_script() {
-	script="/tmp/check_perm-$1"
-	token_format="$2"
-	> "$script"
-	chmod a+x "$script"
-	cat >> "$script" <<-\SH
-	#!/bin/sh
-	usage() { echo "$0: File not found. Usage: $0 FILENAME" >&2; exit 1; }
-	test -f "$1" || usage
-	check() { printf '%s' "$(stat -c'%A' "$1")" | sha256sum | xxd -r -p | base64 | head -c8; } # TODO head/dd # TODO source lib.sh
-	SH
-	cat >> "$script" <<-SH
-	printf '$token_format' "\$(check "\$1")"
-	SH
+# helpers
+mac64() {
+	printf '%s\n' "$1" | sha256sum | xxd -r -p | base64 | take 8;
 }
-make_check_script 1-1 "$token_format"
+
+script='/tmp/check_perm'
+> "$script"
+chmod a+x "$script"
+cat >> "$script" <<-\SH
+#!/bin/sh
+task="$1"; mac="$2"; file="$3"
+if [ "$#" -ne 3 ]; then
+	echo "$0: Usage: $0 TASK MAC FILENAME" >&2
+	exit 1
+fi
+if ! [ -e "$file" ]; then
+	echo "$0: No such file or directory: $file" >&2
+	exit 1
+fi
+check() {
+	stat -c'%A' "$file" |
+		sha256sum |
+		xxd -r -p |
+		base64 |
+		head -c8 # TODO head/dd # TODO source lib.sh
+}
+SH
+cat >> "$script" <<-SH
+printf '%s{%s:%s}' "$COURSE" "\$task:$STUDENT:\$(check "\$file")" "\$mac"
+SH
 
 render_perm() {
 	case "$1" in
@@ -51,10 +56,21 @@ render_perm() {
 		---) echo 'no' ;;
 	esac
 }
-echo "$perms" | cut -c2- | sed 's/.../& /g' | {
-	read -r user group other;
-	task "Enable $(render_perm "$user") permissions for user, $(render_perm "$group") permissions for group, and $(render_perm "$other") permissions for others for the file '$filename' -- then run: check_perm '$filename';"
-}
+
+#####
+
+## perms (textual)
+filename="$(rand_touch)"
+chmod "$(random_perm)" "$filename"
+perms="$(stat -c'%A' "$filename")"
+chmod "$(random_perm)" "$filename"
+
+token_format 1-1 "$(mac64 "$perms")" | while parse_token; do
+	echo "$perms" | cut -c2- | sed 's/.../& /g' | {
+		read -r user group other;
+		task "Enable $(render_perm "$user") permissions for user, $(render_perm "$group") permissions for group, and $(render_perm "$other") permissions for others for the file '$filename' -- then run: check_perm 1-1 $mac '$filename';"
+	}
+done
 
 ## perms (numeric)
 filename="$(rand_touch)"
@@ -63,10 +79,18 @@ perms="$(stat -c'%A' "$filename")"
 perms_octal="$(stat -c'%#a' "$filename")"
 chmod "$(random_perm)" "$filename"
 
-nonce="$(printf '%s' "$perms" | sha256sum | xxd -r -p | base64 | take 8)"
-token_format="$(token_format 1-2 "$nonce")"
+token_format 1-2 "$(mac64 "$perms")" | while parse_token; do
+	task "Set the octal permissions '$perms_octal' for the file '$filename' -- then run: check_perm 1-2 $mac '$filename';"
+done
 
-make_check_script 1-2 "$token_format"
+## perms (symbolic)
+filename="$(rand_touch)"
+chmod "$(random_perm)" "$filename"
+perms="$(stat -c'%A' "$filename")"
+chmod "$(random_perm)" "$filename"
 
-task "Set the octal permissions '0$perms_octal' for the file '$filename' -- then run: check_perm-1-2 $mac '$filename';"
+token_format 1-3 "$(mac64 "$perms")" | while parse_token; do
+	task "Set permissions '${perms#?}' for the file '$filename' -- then run: check_perm 1-3 $mac '$filename';"
+done
 
+## TODO create file or dir, then set perms
