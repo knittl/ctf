7937d15e4a188f18efe7adfc608f8e81b1aa9fba
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2023-12-17 21:58:41 +0100

    Use level/task counters for `cat_simple` tasks

--- cat_simple.sh
+++ cat_simple.sh
@@ -9,14 +9,19 @@ test -d "$root" || mkdir -p "$root" || exit 1
 cd "$root"
 root="$PWD" # get absolute path
 
+
+current_level=1
+
+next_task
 dir="$(rand_mkdir)"
 cd "$dir"
 for _ in $(random_seq 8 16); do touch "$(random_filename)"; done
-echo "$(token 1-1)" > "$(find -type f | pick_random)"
+current_token > "$(find -type f | pick_random)"
 task "Token is content of the only non-empty file in directory '$dir/'"
 cd "$root"
 
-echo "$(token 1-2)" | while parse_token; do
+next_task
+current_token | while parse_token; do
 	printf '%s' "$course" > part0
 	printf '%s' '{' > part1
 	printf '%s' "$exercise" > part2
@@ -30,14 +35,16 @@ echo "$(token 1-2)" | while parse_token; do
 done
 task "Token is in files 'part0' through 'part9', sorted alphabetically"
 
+next_task
 file="$(random_filename) $(random_filename) $(random_filename)"
-token 1-3 > "$file"
+current_token > "$file"
 task 'Token is in file with spaces in its name'
 
+next_task
 prefix="$(random_filename)"
 suffix="$(random_filename)"
 for _ in $(random_seq 8 32); do
-	fake_token 1-4 > "$prefix$(random_filename)$suffix"
+	fake_token "$(level)" > "$prefix$(random_filename)$suffix"
 done
-token 1-4 > "$prefix*$suffix"
+current_token > "$prefix*$suffix"
 task 'Token is in file with asterisk (*) in its name'
