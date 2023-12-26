4597a4b2b9a930a36a10699b0e9795aae5498881
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2023-12-26 19:53:41 +0100

    task: cd

---
+++ tasks/cd.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+. ./lib.sh
+
+init_level
+init_root "$1"
+exec 2> README
+
+next_task
+token_format "$level" "$(mac64 /etc)" | while parse_token; do
+	task "Show a command to change to the 'etc' directory inside the file system root (i.e. '/'). The command has to work independently of your current directory. Get the token by running: $(bold "check cd $level $mac") $(underlined 'your command')"
+done
+
+# TODO relative directory change
+
+next_task
+filename="$(uniq_filename)"
+token_format "$level" "$(mac64 "0/home/$STUDENT/$filename")" | while parse_token; do
+	task "Create an empty file with name '$filename' in directory /home/$STUDENT/. Get the token by running: $(bold "check emptyfile $level $mac") $(underlined "/home/$STUDENT/$filename")"
+done
