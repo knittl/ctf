c5e469245fd6e387eb0c915b6723c5b2bd4f25da
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2023-12-27 21:52:41 +0100

    task/regex: services

--- tasks/regex.sh
+++ tasks/regex.sh
@@ -28,3 +28,23 @@ token_format "$level" "$(mac64 "$largest_number")" | while parse_token; do
 	task "Use a regular expression to find the largest number in file '$file' (NB the file contains negative numbers). Get the token by running: $(bold check printf $level $mac) $(underlined largest_number)"
 done
 )
+
+next_task
+(
+awk '!/^#/&&/tcp/' /etc/services | pick_random | while read -r service port _; do
+	port="${port%/tcp}"
+	token_format "$level" "$(mac64 "$port")" | while parse_token; do
+		task "Which TCP port is associated with the service/protocol '$service'? The file '/etc/services' contains a list of services and their assigned ports. Get the token by running: $(bold check printf $level $mac) $(underlined port_number)"
+	done
+done
+)
+
+next_task
+(
+awk '!/^#/&&/tcp/' /etc/services | pick_random | while read -r service port _; do
+	port="${port%/tcp}"
+	token_format "$level" "$(mac64 "$service")" | while parse_token; do
+		task "Which service/protocol is associated with TCP port $port? The file '/etc/services' contains a list of services and their assigned ports. Get the token by running: $(bold check printf $level $mac) $(underlined service_name)"
+	done
+done
+)
