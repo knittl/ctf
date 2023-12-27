9333d690ed5dee36836d2f23047e548a2cd0785d
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2023-12-27 21:37:26 +0100

    task/cd: pick random dir

--- tasks/cd.sh
+++ tasks/cd.sh
@@ -7,14 +7,22 @@ init_root "$1"
 exec 2> README
 
 next_task
-token_format "$level" "$(mac64 /etc)" | while parse_token; do
-	task "Show a command to change to the 'etc' directory inside the file system root (i.e. '/'). The command has to work independently of your current directory. Get the token by running: $(bold "check cd $level $mac a") $(underlined your command)"
+(
+dir=/
+while [ "$dir" = / ]; do dir="$(find / -maxdepth 1 -type d -perm /o+x | pick_random)"; done
+token_format "$level" "$(mac64 "$dir")" | while parse_token; do
+	task "Show a command to change to the '${dir#/}' directory inside the file system root (i.e. '/'). The command has to work independently of your current directory. Get the token by running: $(bold "check cd $level $mac a") $(underlined your command)"
 done
+)
 
 next_task
-token_format "$level" "$(mac64 /bin)" | while parse_token; do
-	task "The output of $(bold pwd) is '/home/$STUDENT'. Show a command to change to the 'bin' directory inside the file system root (i.e. '/'). The command must use a relative path. Get the token by running: $(bold "check cd $level $mac r") $(underlined 'your command')"
+(
+dir=/
+while [ "$dir" = / ]; do dir="$(find / -maxdepth 1 -type d -perm /o+x | pick_random)"; done
+token_format "$level" "$(mac64 "$dir")" | while parse_token; do
+	task "The output of $(bold pwd) is '/home/$STUDENT'. Show a command to change to the '${dir#/}' directory inside the file system root (i.e. '/'). The command must use a relative path. Get the token by running: $(bold "check cd $level $mac r") $(underlined 'your command')"
 done
+)
 
 next_task
 filename="$(uniq_filename)"
