4063c118f950630c06adc5a8ea30d4d944201b4b
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2023-12-27 10:54:53 +0100

    lib: init level

--- lib.sh
+++ lib.sh
@@ -43,6 +43,9 @@ input() {
 
 take() { dd bs=1 count="$1" 2>/dev/null; } # TODO use head -c"$1"?
 
+init_level() {
+	: "${current_level:?must be set}"
+}
 init_root() {
 	root="${1?root dir missing}"
 	test -d "$root" || mkdir -p "$root" || exit 1
