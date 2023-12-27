dd71d078d2b9f24667edec18e0f31dda970cc2fb
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2023-12-27 11:03:19 +0100

    cd: rephrase task

--- tasks/cd.sh
+++ tasks/cd.sh
@@ -7,8 +7,8 @@ init_root "$1"
 exec 2> README
 
 next_task
-(
 token_format "$level" "$(mac64 /etc)" | while parse_token; do
-	task "From directory /home/$STUDENT/, show a command to change to the 'etc' directory inside the file system root (i.e. '/'). Get the token by running: [4mcheck cd $level $mac your command[0m"
+	task "Show a command to change to the 'etc' directory inside the file system root (i.e. '/'). The command has to work regardless of your current directory. Get the token by running: $(bold "check cd $level $mac") $(underlined 'your command')"
 done
-)
+
+# TODO relative directory change
