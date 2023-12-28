cc7a6ba28372cbf14199075ac0d3712eb4a0a917
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2023-12-28 08:40:35 +0100

    crypto: simplify caesar challenge setup

--- tasks/crypto.sh
+++ tasks/crypto.sh
@@ -10,17 +10,14 @@ next_task
 (
 rot="$(random_int 3 23)"
 file="caesar-$(random_filename)-$rot.txt"
-word="$rot$(random_alpha "$(random_int 8 16)" | to_lower)"
-alphabet=abcdefghijklmnopqrstuvwxyz
+alphabet=ABCDEFGHIJKLMNOPQRSTUVWXYZ
 rotate() { printf '%s%s\n' "$(input "$2" | cut -c"$(($1+1))-")" "$(input "$2" | cut -c"-$1")"; }
 {
-	echo "hello $STUDENT,"
+	echo "HELLO $STUDENT,"
 	echo
-	echo 'you have recovered the secret message!'
-	echo "here is the word for your personal token: $word"
-} | tr 'a-z' "$(rotate "$rot" "$alphabet")" > "$file"
+	echo 'YOU HAVE RECOVERED THE SECRET MESSAGE!'
+	echo "HERE IS YOUR PERSONAL TOKEN: $(current_token)"
+} | tr 'A-Z' "$(rotate "$rot" "$alphabet")" > "$file"
 
-token_format "$level" "$(mac64 "$word")" | while parse_token; do
-	task "You received an encrypted file '$file'. Can you recover its plaintext? (Hint: $(bold tr), Caesar cipher). Get the token by running: $(bold check printf $level $mac) $(underlined "'last word of plaintext'")"
-done
+task "You received an encrypted file '$file'. Can you recover its plaintext? (Hint: $(bold tr), Caesar cipher)."
 )
