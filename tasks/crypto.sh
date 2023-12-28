8fab387a178f9997e54019237d6613b470cde919
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2023-12-28 09:54:04 +0100

    crypto: tune challenge

--- tasks/crypto.sh
+++ tasks/crypto.sh
@@ -9,15 +9,24 @@ exec 2> README
 next_task
 (
 rot="$(random_int 3 23)"
-file="caesar-$(random_filename)-$rot.txt"
+file="CAESAR-$(random_filename)-$rot.TXT"
 alphabet=ABCDEFGHIJKLMNOPQRSTUVWXYZ
 rotate() { printf '%s%s\n' "$(input "$2" | cut -c"$(($1+1))-")" "$(input "$2" | cut -c"-$1")"; }
-{
-	echo "HELLO $STUDENT,"
-	echo
-	echo 'YOU HAVE RECOVERED THE SECRET MESSAGE!'
-	echo "HERE IS YOUR PERSONAL TOKEN: $(current_token)"
-} | tr 'A-Z' "$(rotate "$rot" "$alphabet")" > "$file"
+cat <<EOF | tr 'A-Z' "$(rotate "$rot" "$alphabet")" > "$file"
+HELLO $(input "$STUDENT" | to_upper),
+
+YOU HAVE RECOVERED THE SECRET MESSAGE!
+
+DON'T BE CONFUSED BY THESE FAKE TOKENS: $(fake_token "$level") $(fake_token "$level")
+OR THIS ONE: $(fake_token "$level")
+
+HERE IS YOUR PERSONAL TOKEN: $(current_token)
+
+YOU CAN IGNORE ALL FOLLOWING TOKENS:
+$(fake_token "$level") $(fake_token "$level") $(fake_token "$level")
+$(fake_token "$level")
+$(fake_token "$level") $(fake_token "$level")
+EOF
 
 task "You received an encrypted file '$file'. Can you recover its plaintext? (Hint: $(bold tr), Caesar cipher)."
 )
