6b5a2d4211dc867dc4753e67fcaccd7608241af2
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2024-01-01 15:05:43 +0100

    generate student list with secrets

---
+++ gen-secrets.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. ./lib.sh >/dev/null
+
+if [ "$#" -lt 1 ]; then
+	err "Usage: $0 COURSE [FILE]..."
+	exit 1
+fi
+
+course="$1"; shift
+
+cat "$@" | while read -r student name; do
+	test "$student" || continue
+	test "${student#'#'}" = "$student" || continue # skip comments
+	printf '%s\t%s\t%s\t%s\n' "$course" "$student" "$(random_alnum)" "$name"
+done
