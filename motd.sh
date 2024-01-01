3584233c94d2ad0af73078ac4a5cc7dbf3e8bb0d
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2024-01-01 16:09:03 +0100

    motd: explain how to exit task list

--- motd.sh
+++ motd.sh
@@ -8,7 +8,8 @@ Welcome $(bold "${STUDENTNAME:-$STUDENT}"), these are your personal tasks!
 Your goal is to find tokens, such as $(bold "$(token 0-0)").
 Tokens comprise the task number, your student id, and two special values.
 
-Start by changing into $(bold '~/1-files/') and then typing $(bold show-tasks).
+Start by changing into $(bold "~/$(cd tasks && echo 00-*/)") and then typing $(bold show-tasks).
+Press $(bold q) to quit the task list and to return to the shell prompt.
 
 You can always type $(bold show-motd) to see this message again.
 
