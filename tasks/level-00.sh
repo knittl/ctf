14666f541c1703e003ef73122da374e92fb0fa93
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2024-01-01 23:40:29 +0100

    tasks: use prepare_token

--- tasks/level-00.sh
+++ tasks/level-00.sh
@@ -17,26 +17,23 @@ next_task # 2
 (
 dir=/
 while [ "$dir" = / ]; do dir="$(find / -maxdepth 1 -type d -perm /o+x | pick_random)"; done
-token_format "$level" "$(mac64 "$dir")" | while parse_token; do
-	task "Show a command to change to the '${dir#/}' directory inside the file system root (i.e. '/'). The command has to work independently of your current directory. Get the token by running: $(bold "check cd $level $mac a") $(underlined your command)"
-done
+prepare_current_token "$dir"
+task "Show a command to change to the '${dir#/}' directory inside the file system root (i.e. '/'). The command has to work independently of your current directory. Get the token by running: $(bold "check cd $level $mac a") $(underlined your command)"
 )
 
 next_task # 3
 (
 dir=/
 while [ "$dir" = / ]; do dir="$(find / -maxdepth 1 -type d -perm /o+x | pick_random)"; done
-token_format "$level" "$(mac64 "$dir")" | while parse_token; do
-	task "The output of $(bold pwd) is '/home/$STUDENT'. Show a command to change to the '${dir#/}' directory inside the file system root (i.e. '/'). The command must use a relative path. Get the token by running: $(bold "check cd $level $mac r") $(underlined 'your command')"
-done
+prepare_current_token "$dir"
+task "The output of $(bold pwd) is '/home/$STUDENT'. Show a command to change to the '${dir#/}' directory inside the file system root (i.e. '/'). The command must use a relative path. Get the token by running: $(bold "check cd $level $mac r") $(underlined 'your command')"
 )
 
 next_task # 4
 (
 filename="$(uniq_filename)"
-token_format "$level" "$(mac64 "0/home/$STUDENT/$filename")" | while parse_token; do
-	task "Create an $(bold empty) file with name '$filename' in directory /home/$STUDENT/. Get the token by running: $(bold "check emptyfile $level $mac") $(underlined "/home/$STUDENT/$filename")"
-done
+prepare_current_token "0/home/$STUDENT/$filename"
+task "Create an $(bold empty) file with name '$filename' in directory /home/$STUDENT/. Get the token by running: $(bold "check emptyfile $level $mac") $(underlined "/home/$STUDENT/$filename")"
 )
 
 next_task # 5
@@ -54,9 +51,8 @@ mk_files() {
 
 mk_files jpg jpeg
 
-token_format "$level" "$(ls -l * | mac64)" | while parse_token; do
-	task "How can you list details (size, date, ...) about all (non-hidden) $(bold jpg) and $(bold jpeg) image files in the directory '$dir' with a single command? Get the token by running: $(bold "check details $level $mac") $(underlined your command)"
-done
+prepare_current_token "$(ls -l *)"
+task "How can you list details (size, date, ...) about all (non-hidden) $(bold jpg) and $(bold jpeg) image files in the directory '$dir' with a single command? Get the token by running: $(bold "check details $level $mac") $(underlined your command)"
 
 mk_files png gif jar j2k jbg jfif jiff jpg.gz json
 )
@@ -75,18 +71,16 @@ names=$(
 	done | sort -u
 )
 
-token_format "$level" "$(mac64 "$names")" | while parse_token; do
-	task "How can you save a simple list of the files in directory '$dir' into a file? Execute the command to write the file and then run: $(bold "check cat $level $mac") $(underlined path/to/your/file)"
-done
+prepare_current_token "$names"
+task "How can you save a simple list of the files in directory '$dir' into a file? Execute the command to write the file and then run: $(bold "check cat $level $mac") $(underlined path/to/your/file)"
 )
 
 next_task # 7
 (
 count="$(random_int 4 8)"
 wildcard="$(random_alnum 4)_*/*[$(random_alpha $(random_int 4 8))]/$(printf "%$(random_int 2 4)s" | tr ' ' '?').*"
-token_format "$level" "$(printf '%d\n%s\n' "$count" "$wildcard" | mac64)" | while parse_token; do
-	task "Create a directory which contains $(bold "exactly $count") files which match the wildcard pattern '$wildcard'. Then run $(bold "check glob $level $mac $count '$wildcard'") $(underlined path/to/directory)"
-done
+prepare_current_token "$(printf '%d\n%s\n' "$count" "$wildcard")"
+task "Create a directory which contains $(bold "exactly $count") files which match the wildcard pattern '$wildcard'. Then run $(bold "check glob $level $mac $count '$wildcard'") $(underlined path/to/directory)"
 )
 
 next_task # 8
