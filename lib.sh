c0f4ae036d94214d78538c8a0540091dbfd2eed9
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2024-01-01 23:40:14 +0100

    lib: `prepare_token` -- a replacement for `token_format|while parse_token`

--- lib.sh
+++ lib.sh
@@ -172,6 +172,12 @@ token() {
 token_format() {
 	token "$1" "$3" "$4" "$5" "$2" # pin nonce
 }
+prepare_token() {
+	nonce="$(mac64 "$2")"      # expected nonce
+	token "$1" "$3" "$4" "$5" "$nonce" >/dev/null # pin nonce
+	# output variables: course exercise/task student nonce pepper
+}
+prepare_current_token() { prepare_token "$(level)" "$@"; }
 
 current_token() { token "$(level)"; }
 current_fake_token() { fake_token "$(level)"; }
