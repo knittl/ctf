6adcc1dd62392020d588438fd709b22aeaece7b9
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2024-01-06 23:28:01 +0100

    task descriptions: use large space between sentences

--- tasks/level-05.sh
+++ tasks/level-05.sh
@@ -11,7 +11,7 @@ next_task # 1 static script
 word="$(random_alnum)"
 prepare_current_token "hello $word"
 task <<TASK
-Create an $(bold executable script file) which writes '$(bold "hello $word")' (without quotes) to $(bold standard output). Get the token by running: $(bold "$(print_check exec)") $(underlined ./your_script)
+Create an $(bold executable script file) which writes '$(bold "hello $word")' (without quotes) to $(bold standard output).  Get the token by running: $(bold "$(print_check exec)") $(underlined ./your_script)
 TASK
 )
 
@@ -22,7 +22,7 @@ word1="$(word)"
 word2="$(word)  $(word)"
 prepare_current_token "$(printf 'hello %s\nhello %s\n' "$word1" "$word2")"
 task <<TASK
-Create an $(bold executable script file) which writes '$(underlined "hello XXXX")' (without quotes) to $(bold standard output). ($(underlined XXXX) shall be the $(bold first argument) passed to the script). The script must print arguments with spaces $(bold verbatim) (i.e. ./script 'a  b' outputs 'hello a  b'). Get the token by running: $(bold "$(print_check hello "$word1" "$(word)" "$word2" "$(word)")") $(underlined ./your_script)
+Create an $(bold executable script file) which writes '$(underlined "hello XXXX")' (without quotes) to $(bold standard output).  ($(underlined XXXX) shall be the $(bold first argument) passed to the script).  The script must print arguments with spaces $(bold verbatim) (i.e. ./script 'a  b' outputs 'hello a  b').  Get the token by running: $(bold "$(print_check hello "$word1" "$(word)" "$word2" "$(word)")") $(underlined ./your_script)
 TASK
 )
 
@@ -155,13 +155,13 @@ info "The file '$file' contains a list of pizzas together with their ingredients
 next_task # 3 pizza vegetarian
 repeat "$(random_int 2 8)" pizza current_token vegetables vegetables
 repeat "$(random_int 2 8)" pizza current_fake_token vegetables meats
-task "The token is next to a $(bold vegetarian) pizza. You can find a list of meat-based ingredients in the file $(bold meats)."
+task "The token is next to a $(bold vegetarian) pizza.  You can find a list of meat-based ingredients in the file $(bold meats)."
 
 next_task # 4 pizza extras
 repeat "$(random_int 2 8)" pizza current_token vegetables meats "$yummy"
 repeat "$(random_int 2 4)" pizza current_fake_token meats "$yucky" "$yummy"
 repeat "$(random_int 2 4)" pizza current_fake_token vegetables "$yucky" "$yummy"
-task "You cannot decide which pizza to order. You $(bold love) $(pretty "$yummy"); but you $(bold can not) stand $(pretty "$yucky"). The token is next to your favorite pizzas."
+task "You cannot decide which pizza to order.  You $(bold love) $(pretty "$yummy"); but you $(bold can not) stand $(pretty "$yucky").  The token is next to your favorite pizzas."
 } | shuf | paste "$names" - | grep -e "$COURSE{.*}" > "$file"
 )
 repeat 2 next_task # re-apply task from subshell to parent shell
