1cb3a9ae8421316e4a7d2c13b821b27a1525918f
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2025-12-14 15:53:55 +0100

    WIP explain tutor mode

--- bin/motd.sh
+++ bin/motd.sh
@@ -2,16 +2,25 @@
 
 . ./lib.sh >/dev/null
 
+# TODO only write check-tokens hint when "tutormode" is enabled
+
+token0="$(token 0-0)"
+faketoken0="$(fake_token 0-0)"
 cat <<EOF
 Welcome $(bold "${STUDENTNAME:-$STUDENT}"), these are your personal tasks!
 
-Your goal is to find tokens, such as $(bold "$(token 0-0)").
+Your goal is to find tokens, such as $(bold "$token0").
 Tokens comprise the task number, your student id, and two special values.
 
-Not all tokens are valid. For instance, $(bold "$(fake_token 0-0)")
+Not all tokens are valid. For instance, $(bold "$faketoken0")
 is an invalid token.
 
-Start by changing into $(bold "~/$(cd tasks && echo 00-*/)") and then typing $(bold show-tasks).
+You can verify tokens by running $(bold check-token). Try it out:
+
+    \$ $(bold check-token "$token0")
+    \$ $(bold check-token "$faketoken0")
+
+Start by changing into $(bold "~/$(cd tasks && echo 00-*/)") and then type $(bold show-tasks).
 Press $(bold q) to quit the task list and to return to the shell prompt.
 
 You can always type $(bold show-motd) to see this message again.
@@ -20,3 +29,6 @@ $(fmt dim "Timestamp: $(date -u +%FT%TZ)")
 $(fmt dim "Checksum:  $(printf '%s' "$TOKEN_PEPPER" | sha256sum | cut -c-64)")
 
 EOF
+
+# TODO checksum should change if container is regenerated (???)
+
